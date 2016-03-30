36636b7e2e48063a21a12a221f6cda73fdff89aa
ArkadiySh <arkadiy.shukh@gmail.com>
2016-03-30 23:04:23 +0300

    js task-6 global fix

--- js-module-6/js/script.js
+++ js-module-6/js/script.js
@@ -1,156 +1,155 @@
 var body = document.body;
 
 var start = document.querySelector('.start');
-var reset = document.querySelector('.stop');
-var sec = document.querySelector('.secs');
-var min = document.querySelector('.mins');
-var mil = document.querySelector('.milis');
-var pause = document.querySelector('.pause');
+var stop = document.querySelector('.stop');
+var reset = document.querySelector('.reset');
 var lap = document.querySelector('.lap');
 
-var milliseconds = 0
-var seconds = 0;
-var minutes = 0;
+var lapContainer = document.querySelector('.lapContainer');
 
-function altButton() {
+var mil = document.querySelector('.milis');
+var sec = document.querySelector('.secs');
+var min = document.querySelector('.mins');
+
+// Create blocks for time markers
+function createTimeBlock(type) {
+  lapBlock = document.createElement('div');
+  lapBlock.classList.add('lapBlock');
+  lapContainer.appendChild(lapBlock);
+  var lapText = document.createElement('div');
+
+  lapText.classList.add('lapText');
+  lapBlock.appendChild(lapText);
+  lapText.innerHTML = (type);
+
+  var lapMin = document.createElement('div');
+  lapMin.classList.add('lapSection');
+  lapBlock.appendChild(lapMin);
+  lapMin.innerHTML = (minutes);
+
+  var lapDiv = document.createElement('div');
+  lapDiv.classList.add('lapSection');
+  lapBlock.appendChild(lapDiv);
+  lapDiv.innerHTML = (':');
+
+  var lapSec = document.createElement('div');
+  lapSec.classList.add('lapSection');
+  lapBlock.appendChild(lapSec);
+  lapSec.innerHTML = (seconds);
+
+  var lapDiv = document.createElement('div');
+  lapDiv.classList.add('lapSection');
+  lapBlock.appendChild(lapDiv);
+  lapDiv.innerHTML = (':');
+
+  var lapMilis = document.createElement('div');
+  lapMilis.classList.add('lapSection');
+  lapBlock.appendChild(lapMilis);
+  lapMilis.innerHTML = (milliseconds);
+}
+
+// hide/display START/STOP buttons
+function displayStopButton() {
   start.style.display = 'none';
-  pause.style.display = 'block';
+  stop.style.display = 'block';
 }
 
-function reStart() {
+function displayStartButton() {
   start.style.display = 'block';
-  pause.style.display = 'none';
+  stop.style.display = 'none';
 }
+
+
+// Get Date start point
+function startStopwatch() {
+  initialDate = new Date;
+}
+
+
+
+function getTime() {
+
+  var currentDate = new Date;
+  var timer = currentDate - initialDate;
+
+  milliseconds = (timer % 1000);
+  timer -= milliseconds;
+  milliseconds = Math.floor(milliseconds / 10);
+  timer = Math.floor(timer / 1000);
+
+  seconds = Math.floor (timer % 60);
+  timer -= seconds;
+  timer = Math.floor(timer / 60);
+
+  minutes = Math.floor (timer % 60);
+  timer -= minutes;
+  timer = Math.floor(timer / 60);
+
+  if(milliseconds < 100){
+    milliseconds = '0' + milliseconds;
+  }
+  if(seconds < 10){
+    seconds = '0' + seconds;
+  }
+  if (minutes < 10){
+    minutes = '0' + minutes;
+  }
+}
+
 
 function counter() {
+  getTime();
   mil.innerHTML = milliseconds;
-  min.innerHTML = minutes;
   sec.innerHTML = seconds;
-
-  milliseconds = milliseconds + 200;
-  
-  if (milliseconds > 999) {
-    milliseconds = 0;
-    seconds++;
-  };
-
-  if (seconds > 59) {
-    seconds = 0;
-    minutes++;
-  };
-  
+  min.innerHTML = minutes;
 }
 
-function timer() {
-  timerId = setInterval(counter, 200);
+
+function displayTimer() {
+  timerId = setInterval(counter, 10);
 }
 
+
+function pauseStopwatch() {
+  start.style.display = 'block';
+  stop.style.display = 'none';
+}
+
+
+function newLap() {
+  var currentDate = new Date;
+  var timer = currentDate - initialDate;
+  getTime();
+  createTimeBlock('LAP')
+}
 
 
 function stopTimer() {
   clearInterval(timerId);
-
-   lapBlock = document.createElement('div');
-  lapBlock.classList.add('lapBlock');
-  body.appendChild(lapBlock);
-
-  var lapText = document.createElement('div');
-  lapText.classList.add('lapText');
-  lapBlock.appendChild(lapText);
-  lapText.innerHTML = ('Stop');
-
-  var lapMin = document.createElement('div');
-  lapMin.classList.add('lapSection');
-  lapBlock.appendChild(lapMin);
-  lapMin.innerHTML = (minutes);
-
-  var lapDiv = document.createElement('div');
-  lapDiv.classList.add('lapSection');
-  lapBlock.appendChild(lapDiv);
-  lapDiv.innerHTML = (':');
-
-  var lapSec = document.createElement('div');
-  lapSec.classList.add('lapSection');
-  lapBlock.appendChild(lapSec);
-  lapSec.innerHTML = (seconds);
-
-  var lapDiv = document.createElement('div');
-  lapDiv.classList.add('lapSection');
-  lapBlock.appendChild(lapDiv);
-  lapDiv.innerHTML = (':');
-
-  var lapMilis = document.createElement('div');
-  lapMilis.classList.add('lapSection');
-  lapBlock.appendChild(lapMilis);
-  lapMilis.innerHTML = (milliseconds);
+  var currentDate = new Date;
+  var timer = currentDate - initialDate;
+  getTime();
+  createTimeBlock('STOP');
 }
-
-
-
-function newLap() {
-   lapBlock = document.createElement('div');
-  lapBlock.classList.add('lapBlock');
-  body.appendChild(lapBlock);
-
-  var lapText = document.createElement('div');
-  lapText.classList.add('lapText');
-  lapBlock.appendChild(lapText);
-  lapText.innerHTML = ('Lap');
-
-  var lapMin = document.createElement('div');
-  lapMin.classList.add('lapSection');
-  lapBlock.appendChild(lapMin);
-  lapMin.innerHTML = (minutes);
-
-  var lapDiv = document.createElement('div');
-  lapDiv.classList.add('lapSection');
-  lapBlock.appendChild(lapDiv);
-  lapDiv.innerHTML = (':');
-
-  var lapSec = document.createElement('div');
-  lapSec.classList.add('lapSection');
-  lapBlock.appendChild(lapSec);
-  lapSec.innerHTML = (seconds);
-
-  var lapDiv = document.createElement('div');
-  lapDiv.classList.add('lapSection');
-  lapBlock.appendChild(lapDiv);
-  lapDiv.innerHTML = (':');
-
-  var lapMilis = document.createElement('div');
-  lapMilis.classList.add('lapSection');
-  lapBlock.appendChild(lapMilis);
-  lapMilis.innerHTML = (milliseconds);
-}
-
 
 
 function resetTimer() {
   clearInterval(timerId);
-
   start.style.display = 'block';
-  pause.style.display = 'none';
-
-  milliseconds = 0;
-  seconds = 0;
-  minutes = 0;
-
-  mil.innerHTML = milliseconds;
-  min.innerHTML = minutes;
-  sec.innerHTML = seconds;
-
-  body.removeChild(lapBlock);
+  stop.style.display = 'none';
+  mil.innerHTML = '00';
+  min.innerHTML = '00';
+  sec.innerHTML = '00';
+  document.querySelector('.lapContainer').innerHTML = '';
 }
 
+start.addEventListener('click', startStopwatch);
+start.addEventListener('click', displayStopButton);
+start.addEventListener('click', displayTimer);
 
-
-start.addEventListener('click', timer);
-start.addEventListener('click', altButton);
-pause.addEventListener('click', reStart)
 lap.addEventListener('click', newLap)
 
+stop.addEventListener('click', stopTimer)
+stop.addEventListener('click', displayStartButton);
 
-
-pause.addEventListener('click', stopTimer);
 reset.addEventListener('click', resetTimer);
-
