d64828082c519c8197749465cdf77f600689dc46
ArkadiySh <arkadiy.shukh@gmail.com>
2016-04-13 23:09:59 +0300

    js task-10 minor fix

--- js-module-10/js/script.js
+++ js-module-10/js/script.js
@@ -3,13 +3,22 @@ var submenu = document.querySelector('.submenu');
 var submenu2 = document.querySelector('.submenu2');
 var submenuDropdown = document.querySelector('.submenu__dropdown');
 
+// var submenuItem = document.querySelectorAll('.submenu__item');
+// console.log(submenuItem);
+// console.log(submenuItem.length);
+
 function displaySubmenu(el) {
   var height = 0;
+
+  // console.log(submenuItem);
+  // var itemHeight = parseInt(submenuItem.style.height);
+  // console.log(itemHeight);
 
 	if (el.style.height == 0 || el.style.height == '0px') {
 		var displayInt = setInterval(function(){
 			height += 15;
 			el.style.height = height + 'px'
+      // if (height > (itemHeight * submenuItem.length) {
 			if (height > 250){
 				clearInterval(displayInt);
 			};	
