21074e54142ccae872e4b77b9e59a77636da487d
ArkadiySh <arkadiy.shukh@gmail.com>
2016-04-13 22:55:48 +0300

    js task-10 initial commit

---
+++ js-module-10/js/script.js
@@ -0,0 +1,48 @@
+var dropdown = document.querySelector('.dropdown');
+var submenu = document.querySelector('.submenu');
+var submenu2 = document.querySelector('.submenu2');
+var submenuDropdown = document.querySelector('.submenu__dropdown');
+
+function displaySubmenu(el) {
+  var height = 0;
+
+	if (el.style.height == 0 || el.style.height == '0px') {
+		var displayInt = setInterval(function(){
+			height += 15;
+			el.style.height = height + 'px'
+			if (height > 250){
+				clearInterval(displayInt);
+			};	
+		}, 10);
+	};
+};
+
+function hideSubmenu(el){
+  var height = parseInt(el.style.height);
+
+  if (parseInt(el.style.height) > 0) {
+    var hideInt = setInterval(function(){
+      height -= 15;
+      el.style.height = height + 'px'
+      if (height < 1){
+        clearInterval(hideInt);
+      };  
+    }, 10);
+  };
+};
+
+dropdown.addEventListener('mouseover', function(){
+	displaySubmenu(submenu);
+});
+
+dropdown.addEventListener('mouseleave', function(){
+	hideSubmenu(submenu);
+});
+
+submenuDropdown.addEventListener('mouseover', function(){
+  displaySubmenu(submenu2);
+});
+
+submenuDropdown.addEventListener('mouseleave', function(){
+  hideSubmenu(submenu2);
+});
