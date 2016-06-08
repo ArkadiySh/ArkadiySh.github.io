85c48c3b779757b515883f3acc88ad1766b92eac
ArkadiySh <arkadiy.shukh@gmail.com>
2016-06-08 19:59:41 +0300

    exam test commit

---
+++ EXAM/js/src/script.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  var container = document.querySelector('#container');
+  var msnry = new Masonry( container, {
+    // Настройки
+    columnWidth: 200,
+    itemSelector: '.ideas__link'
+  });
+
+});
