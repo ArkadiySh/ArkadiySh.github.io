139c8bd7098c423dc2d61d3b8b4a035d53d62179
ArkadiySh <arkadiy.shukh@gmail.com>
2016-06-18 16:35:26 +0300

    dropdown made

--- in_progress/abant/js/src/script.js
+++ in_progress/abant/js/src/script.js
@@ -1,11 +1,37 @@
-function Model(argument) {
-	// body...
-}
+$(document).ready(function(){
+  var menu = $('.menu-device__menu');
 
-function View(argument) {
-	// body...
-}
+  $('.menu__button').click(
 
-function Controller(argument) {
-	// body...
-}
+    function() {
+      $(this).find(menu).slideToggle('slow');
+    }
+  );
+
+
+
+
+
+
+  $('.menu-device__item').hover(
+    function(){
+      var n = $(this).find('.submenu__item').length;
+
+      if (n > 0) {
+        $(this).height((n + 1)  * 35);
+      } else ( $(this).height(35) );
+
+      $(this).find('.submenu').slideDown('fast');
+    },
+
+    function(){
+      $(this).height(35);
+      $(this).find('.submenu').slideUp('fast');
+
+    }
+
+
+
+    )
+
+})
