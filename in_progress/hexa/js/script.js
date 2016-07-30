03bae277ca0825defa01250935ff8c26d4fc9420
ArkadiySh <arkadiy.shukh@gmail.com>
2016-07-30 13:08:15 +0300

    hexa reserve commit

--- in_progress/hexa/js/script.js
+++ in_progress/hexa/js/script.js
@@ -2,39 +2,40 @@ var $right = $('.slider__arrow--right');
 var $left = $('.slider__arrow--left');
 var $dot = $('.slider__dot');
 
-var room = ['Для спальни', 'Для комнаты', 'Для кухни'];
-var price = [20,30,40];
+var room = ['Для зала', 'Для комнаты', 'Для кухни', 'Для спальни'];
+var price = [50, 20,30,40];
 
-$dot.on('click', function(){
-	$dot.removeClass('slider__dot--active');
-	$(this).addClass('slider__dot--active');
+
+var i = 0;
+$right.on('click', function(){
+	if (i < ($dot.length - 1)) {
+		i++;
+		$('.main-wrapper').css('background-image', 'url(../../img/room-' + i + '.jpg)');
+		$dot.removeClass('slider__dot--active');
+		$dot[i].classList.add('slider__dot--active');
+		$('.slider__room > p').html(room[i]);
+		$('.slider__price > p > span').html(price[i]);
+	};
+})
+
+$left.on('click', function(){
+	if (i > 0) {
+		i--;
+		$('.main-wrapper').css('background-image', 'url(../../img/room-' + i + '.jpg)');
+		$dot.removeClass('slider__dot--active');
+		$dot[i].classList.add('slider__dot--active');
+		$('.slider__room > p').html(room[i]);
+		$('.slider__price > p > span').html(price[i]);
+	};
 })
 
 
-function slider () {
-	var i = 0;
-
-	$right.on('click', function(){
-		if (i < ($dot.length - 1)) {
-			$('.main-wrapper').css('background-image', 'url(../../img/room-' + i + '.jpg)');
-			$dot.removeClass('slider__dot--active');
-			$dot[i+1].classList.add('slider__dot--active');
-			$('.slider__room > p').html(room[i]);
-			$('.slider__price > p > span').html(price[i]);
-			i++;
-		};
-	})
-
-	$left.on('click', function(){
-		if (i > -1) {
-			$('.main-wrapper').css('background-image', 'url(../../img/room-' + i + '.jpg)');
-			$dot.removeClass('slider__dot--active');
-			$dot[i-1].classList.add('slider__dot--active');
-			$('.slider__room > p').html(room[i]);
-			$('.slider__price > p > span').html(price[i]);
-			i--;
-		};
-	})
-}
-
-slider();
+$dot.on('click', function () {
+	// body...
+	$dot.removeClass('slider__dot--active');
+	$(this).addClass('slider__dot--active');
+	i = $(this).index();
+	$('.slider__room > p').html(room[i]);
+	$('.slider__price > p > span').html(price[i]);
+	$('.main-wrapper').css('background-image', 'url(../../img/room-' + i + '.jpg)');
+})
