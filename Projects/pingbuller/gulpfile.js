8571fa73171bdbe76b4745dd6c0c7ec2bcff371d
ArkadiySh <arkadiy.shukh@gmail.com>
2016-08-08 22:35:23 +0300

    pb pre-commit

--- Projects/pingbuller/gulpfile.js
+++ Projects/pingbuller/gulpfile.js
@@ -6,21 +6,13 @@ var rename      = require('gulp-rename');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
 var lec         = require ('gulp-line-ending-corrector');
+var jsmin       = require('gulp-jsmin');
 
 
-// CSS CONCATENATION
-gulp.task('concat-css', ['sass'], function () { // runs SASS before CONCAT-CSS
-  return gulp.src([
-    'app/css/sass/*.scss',
-    ])
-    .pipe(concatCss("style.scss"))
-    .pipe(lec({verbose:true, eolc: 'CRLF'}))
-    .pipe(gulp.dest('app/css/sass/'));
-});
 
 // SCSS ========>>>>>>> CSS
 gulp.task('sass', function(){ 
-    return gulp.src('app/css/sass/style.scss') 
+    return gulp.src('app/css/sass/*.scss') 
         .pipe(sass())
         .pipe(lec({verbose:true, eolc: 'CRLF'}))
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
@@ -28,6 +20,18 @@ gulp.task('sass', function(){
 });
 
 
+// CSS CONCATENATION
+gulp.task('concat-css', ['sass'], function () { // runs SASS before CONCAT-CSS
+  return gulp.src([
+    'app/css/reset.css',
+    'app/css/320.css',
+    'app/css/480.css',
+    'app/css/768.css',
+    'app/css/960.css',
+    ])
+    .pipe(concatCss("style.css"))
+    .pipe(gulp.dest('app/css/'));
+});
 
 
 // MAIN CSS MINIFICATION
@@ -40,6 +44,14 @@ gulp.task('cssmin', ['concat-css'], function () {
 });
 
 
+gulp.task('jsmin', function () {
+    gulp.src('app/js/script.js')
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/js/'));
+});
+
+
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
@@ -49,7 +61,7 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('watch', ['cssmin', 'browser-sync'], function() {
+gulp.task('watch', [ 'jsmin', 'cssmin', 'browser-sync'], function() {
     gulp.watch('app/css/sass/*.scss', ['cssmin']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
